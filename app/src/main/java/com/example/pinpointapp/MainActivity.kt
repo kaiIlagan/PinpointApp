4ff3479f20956db3ff0a9e1d6bd2619ee2da10f0
kaiIlagan <kayinilagan@gmail.com>
2022-10-05 20:31:40 -0400

    Going through Free Plan for project, got uploading, retrieving, and modifying through Backendless working.

--- app/src/main/java/com/example/pinpointapp/MainActivity.kt
+++ app/src/main/java/com/example/pinpointapp/MainActivity.kt
@@ -1,43 +1,99 @@
 package com.example.pinpointapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pinpointapp.ui.theme.PinpointAppTheme
+import com.backendless.Backendless
+import com.backendless.async.callback.AsyncCallback
+import com.backendless.exceptions.BackendlessFault
+import com.example.pinpointapp.domain.model.Person
+import com.example.pinpointapp.keys.Keys.API_KEY
+import com.example.pinpointapp.keys.Keys.APP_ID
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PinpointAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+        Backendless.initApp(this, APP_ID, API_KEY)
+
+        //saveAPerson((floor(Math.random() * 100)).toInt(), "Amber");
+        //retrieveAPerson();
+        updateAPerson()
+
+    }
+}
+
+ fun saveAPerson(age: Int, name: String) {
+    val objToSave = Person(age, name)
+
+    Backendless.Data.of(Person::class.java).save(objToSave, object: AsyncCallback<Person>{
+        override fun handleResponse(person: Person )
+        {
+            // new Person instance has been saved
+        }
+
+        override fun handleFault(fault: BackendlessFault )
+        {
+            // an error has occurred, the error code can be retrieved with fault.getCode()
+        }
+    });
+}
+
+fun retrieveAPerson() {
+    var objectRetrieved: Person
+    val tag = "RetrievalPerson"
+
+    Backendless.Data.of(Person::class.java).findFirst(object: AsyncCallback<Person>{
+        override fun handleResponse(response: Person?) {
+            if (response != null) {
+                objectRetrieved = response
+                Log.d(tag, "Successfully received Person from Backendless")
+                objectRetrieved.name?.let { Log.d(tag, it) }
+                objectRetrieved.age?.let { Log.d(tag, it.toString())}
+            } else {
+                Log.e(tag, "Request went through, did not receive a response")
             }
         }
-    }
+
+        override fun handleFault(fault: BackendlessFault?) {
+            Log.e(tag, fault.toString())
+        }
+
+    });
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun updateAPerson() {
+    var person: Person;
+    val tag = "UpdatePerson";
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PinpointAppTheme {
-        Greeting("Android")
-    }
+    Backendless.Data.of(Person::class.java).findFirst(object: AsyncCallback<Person>{
+        override fun handleResponse(response: Person?) {
+            if (response != null) {
+                person = response
+                person.age = (Math.random() * 100).roundToInt()
+
+                //Save Person
+                Backendless.Data.of(Person::class.java).save(person, object: AsyncCallback<Person>{
+                    override fun handleResponse(response: Person?) {
+                        Log.d(tag, "Person has been saved and updated!")
+                        person.name?.let { Log.d(tag, it) }
+                        person.age?.let { Log.d(tag, it.toString()) }
+                    }
+
+                    override fun handleFault(fault: BackendlessFault?) {
+                        Log.e(tag, fault.toString())
+                    }
+
+                })
+            } else {
+                Log.e(tag, "Received a response, but was null")
+            };
+        }
+
+        override fun handleFault(fault: BackendlessFault?) {
+            Log.e(tag, fault.toString())
+        }
+
+    });
 }
---
+++ app/src/main/java/com/example/pinpointapp/domain/model/Person.kt
@@ -0,0 +1,8 @@
+package com.example.pinpointapp.domain.model
+
+data class Person(
+    var age: Int? = null,
+    var name: String? = null,
+    var objectId: String? = null,
+    var ownerId: String? = null
+)
