a62fe01c7cc6cb88252393bce4639f65bbcc93bf
Kayin <kayinilagan@gmail.com>
2022-10-12 21:17:12 -0400

    Removed redudant semicolons

--- app/src/main/java/com/example/pinpointapp/MainActivity.kt
+++ app/src/main/java/com/example/pinpointapp/MainActivity.kt
@@ -30,10 +30,10 @@ class MainActivity : ComponentActivity() {
 }
 
 
-fun loginUser(email: String, password: String){
+fun loginUser(email: String, password: String) {
     val tag = "UserLogin"
 
-    Backendless.UserService.login(email, password, object: AsyncCallback<BackendlessUser>{
+    Backendless.UserService.login(email, password, object : AsyncCallback<BackendlessUser> {
         override fun handleResponse(response: BackendlessUser?) {
             Log.d(tag, "Successfully logged in User")
         }
@@ -46,16 +46,16 @@ fun loginUser(email: String, password: String){
 
 }
 
-fun registerUser(email: String, password: String){
+fun registerUser(email: String, password: String) {
     val tag = "UserRegistration"
     val user = BackendlessUser()
     user.email = email
     user.password = password
 
 
-    Backendless.UserService.register( user, object: AsyncCallback<BackendlessUser>{
+    Backendless.UserService.register(user, object : AsyncCallback<BackendlessUser> {
         override fun handleResponse(response: BackendlessUser?) {
-           Log.d(tag, "Successfully registered User")
+            Log.d(tag, "Successfully registered User")
         }
 
         override fun handleFault(fault: BackendlessFault?) {
@@ -66,33 +66,31 @@ fun registerUser(email: String, password: String){
 }
 
 
- fun saveAPerson(age: Int, name: String) {
+fun saveAPerson(age: Int, name: String) {
     val objToSave = Person(age, name)
 
-    Backendless.Data.of(Person::class.java).save(objToSave, object: AsyncCallback<Person>{
-        override fun handleResponse(person: Person )
-        {
+    Backendless.Data.of(Person::class.java).save(objToSave, object : AsyncCallback<Person> {
+        override fun handleResponse(person: Person) {
             // new Person instance has been saved
         }
 
-        override fun handleFault(fault: BackendlessFault )
-        {
+        override fun handleFault(fault: BackendlessFault) {
             // an error has occurred, the error code can be retrieved with fault.getCode()
         }
-    });
+    })
 }
 
 fun retrieveAPerson() {
     var objectRetrieved: Person
     val tag = "RetrievalPerson"
 
-    Backendless.Data.of(Person::class.java).findFirst(object: AsyncCallback<Person>{
+    Backendless.Data.of(Person::class.java).findFirst(object : AsyncCallback<Person> {
         override fun handleResponse(response: Person?) {
             if (response != null) {
                 objectRetrieved = response
                 Log.d(tag, "Successfully received Person from Backendless")
                 objectRetrieved.name?.let { Log.d(tag, it) }
-                objectRetrieved.age?.let { Log.d(tag, it.toString())}
+                objectRetrieved.age?.let { Log.d(tag, it.toString()) }
             } else {
                 Log.e(tag, "Request went through, did not receive a response")
             }
@@ -102,69 +100,71 @@ fun retrieveAPerson() {
             Log.e(tag, fault.toString())
         }
 
-    });
+    })
 }
 
 fun updateAPerson() {
     var person: Person
     val tag = "UpdatePerson"
 
-    Backendless.Data.of(Person::class.java).findFirst(object: AsyncCallback<Person>{
+    Backendless.Data.of(Person::class.java).findFirst(object : AsyncCallback<Person> {
         override fun handleResponse(response: Person?) {
             if (response != null) {
                 person = response
                 person.age = (Math.random() * 100).roundToInt()
 
                 //Save Person
-                Backendless.Data.of(Person::class.java).save(person, object: AsyncCallback<Person>{
-                    override fun handleResponse(response: Person?) {
-                        Log.d(tag, "Person has been saved and updated!")
-                        person.name?.let { Log.d(tag, it) }
-                        person.age?.let { Log.d(tag, it.toString()) }
-                    }
+                Backendless.Data.of(Person::class.java)
+                    .save(person, object : AsyncCallback<Person> {
+                        override fun handleResponse(response: Person?) {
+                            Log.d(tag, "Person has been saved and updated!")
+                            person.name?.let { Log.d(tag, it) }
+                            person.age?.let { Log.d(tag, it.toString()) }
+                        }
 
-                    override fun handleFault(fault: BackendlessFault?) {
-                        Log.e(tag, fault.toString())
-                    }
+                        override fun handleFault(fault: BackendlessFault?) {
+                            Log.e(tag, fault.toString())
+                        }
 
-                })
+                    })
             } else {
                 Log.e(tag, "Received a response, but was null")
-            };
+            }
         }
 
         override fun handleFault(fault: BackendlessFault?) {
             Log.e(tag, fault.toString())
         }
 
-    });
+    })
 }
 
-fun deleteFirstPerson(){
+fun deleteFirstPerson() {
     var person: Person
     val tag = "DeleteFirstPerson"
 
-    Backendless.Data.of(Person::class.java).findFirst(object: AsyncCallback<Person> {
+    Backendless.Data.of(Person::class.java).findFirst(object : AsyncCallback<Person> {
         override fun handleResponse(response: Person?) {
-            if (response != null){
+            if (response != null) {
                 person = response
-                Backendless.Data.of(Person::class.java).remove(person, object: AsyncCallback<Long>{
-                    override fun handleResponse(response: Long?) {
-                        Log.d(tag, "Successfully removed " + person.name + " " + person.age)
-                    }
+                Backendless.Data.of(Person::class.java)
+                    .remove(person, object : AsyncCallback<Long> {
+                        override fun handleResponse(response: Long?) {
+                            Log.d(tag, "Successfully removed " + person.name + " " + person.age)
+                        }
 
-                    override fun handleFault(fault: BackendlessFault?) {
-                        Log.e(tag, fault.toString())
-                    }
+                        override fun handleFault(fault: BackendlessFault?) {
+                            Log.e(tag, fault.toString())
+                        }
 
-                });
+                    })
             } else {
                 Log.d(tag, "Got a response, however it was null, database may be empty")
             }
         }
 
         override fun handleFault(fault: BackendlessFault?) {
-           Log.e(tag, fault.toString())
+            Log.e(tag, fault.toString())
         }
     })
 }
