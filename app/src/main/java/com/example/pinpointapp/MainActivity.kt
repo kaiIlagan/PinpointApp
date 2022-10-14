ef00ccdfa6682b08ab51dfcb7e086515ff5abcc6
kaiIlagan <kayinilagan@gmail.com>
2022-10-13 20:30:18 -0400

    Added setContent with PinPointAppTheme as well as Screen.kt with objects for planned screens

--- app/src/main/java/com/example/pinpointapp/MainActivity.kt
+++ app/src/main/java/com/example/pinpointapp/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.pinpointapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import com.backendless.Backendless
 import com.example.pinpointapp.keys.Keys.API_KEY
 import com.example.pinpointapp.keys.Keys.APP_ID
+import com.example.pinpointapp.ui.theme.PinpointAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         Backendless.initApp(this, APP_ID, API_KEY)
+
+        setContent{
+            PinpointAppTheme{
+
+            }
+        }
 
     }
 }
