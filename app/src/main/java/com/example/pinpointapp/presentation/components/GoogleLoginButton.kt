6709521d457909f51ec0e1c7798941688df71dde
Kayin <kayinilagan@gmail.com>
2022-10-13 22:10:17 -0400

    Iteration one of GoogleLoginButton.kt with Compose

--- app/src/main/java/com/example/pinpointapp/presentation/components/GoogleLoginButton.kt
+++ app/src/main/java/com/example/pinpointapp/presentation/components/GoogleLoginButton.kt
@@ -1,8 +1,90 @@
 package com.example.pinpointapp.presentation.components
 
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Login
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun GoogleLoginButton() {
+fun GoogleLoginButton(
+    modifier: Modifier = Modifier,
+    loadingState: Boolean = false,
+    primaryText: String = "Sign in with Google",
+    secondaryText: String = "Please wait...",
+    shape: Shape = MaterialTheme.shapes.medium,
+    borderColor: Color = Color.LightGray,
+    borderStrokeWidth: Dp = 1.dp,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    progressIndicatorColor: Color = Color.Blue,
+    onClick: () -> Unit
+) {
+    var buttonText by remember {
+        mutableStateOf(primaryText)
+    }
 
+    LaunchedEffect(key1 = loadingState) {
+        buttonText = if (loadingState) secondaryText else primaryText
+    }
+
+    Surface(
+        modifier = modifier.clickable(enabled = !loadingState) { onClick() },
+        shape = shape,
+        border = BorderStroke(width = borderStrokeWidth, color = borderColor),
+        color = backgroundColor
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.Login,
+                contentDescription = "Login Icon",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(text = buttonText)
+            if (loadingState) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier.size(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun GoogleLoginButtonPreview() {
+    GoogleLoginButton {}
+}
+
+@Composable
+@Preview
+fun GoogleLoginButtonLoadingPreview() {
+    GoogleLoginButton(loadingState = true) {}
 }
